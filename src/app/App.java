7cd8022d198e0d2bfa6bba5e08794a7575ba0d43
evanildo <evanildo.sf91@gmail.com>
2023-10-07 21:37:50 -0300

    delete implementation

--- src/app/App.java
+++ src/app/App.java
@@ -1,10 +1,7 @@
 package app;
 
-import model.entities.Department;
-
 public class App {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
+
     }
 }
