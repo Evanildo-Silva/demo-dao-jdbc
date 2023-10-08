480aabd8c6ece3759c5407a0b938c4f4287983e0
evanildo <evanildo.sf91@gmail.com>
2023-10-07 21:29:38 -0300

    update implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -43,9 +43,9 @@ public class SellerDaoJDBC implements SellerDao {
       st.setDate(3, new java.sql.Date(seller.getBirthDate().getTime()));
       st.setDouble(4, seller.getBaseSalary());
       st.setInt(5, seller.getDepartment().getId());
-
+      
       int rowsAffected = st.executeUpdate();
-
+      
       if (rowsAffected > 0) {
         ResultSet rs = st.getGeneratedKeys();
         if (rs.next()) {
@@ -62,11 +62,31 @@ public class SellerDaoJDBC implements SellerDao {
       DB.closeStatement(st);
     }
   }
-
+  
   @Override
   public void update(Seller seller) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'update'");
+    PreparedStatement st = null;
+    try {
+      st =  conn.prepareStatement(
+        "UPDATE seller "
+        + "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+        + "WHERE Id = ?"
+        );
+        
+        st.setString(1, seller.getName());
+        st.setString(2, seller.getEmail());
+        st.setDate(3, new java.sql.Date(seller.getBirthDate().getTime()));
+        st.setDouble(4, seller.getBaseSalary());
+        st.setInt(5, seller.getDepartment().getId());
+        st.setInt(6, seller.getId());
+
+        st.executeUpdate();
+
+    } catch (SQLException e) {
+      throw new DbException(e.getMessage());
+    } finally {
+      DB.closeStatement(st);
+    }
   }
 
   @Override
