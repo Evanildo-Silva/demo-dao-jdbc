6a9034f026a7ff1e53d85c250036bab6f2d844ff
evanildo <evanildo.sf91@gmail.com>
2023-10-08 21:43:19 -0300

    findAll implementation

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
@@ -23,8 +24,7 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
   @Override
   public void insert(Department department) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    
   }
 
   @Override
@@ -64,8 +64,27 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
   @Override
   public List<Department> findAll() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+     PreparedStatement st = null;
+    ResultSet rs = null;
+    try {
+      st = conn.prepareStatement("SELECT * FROM department ORDER BY Name");
+
+      rs = st.executeQuery();
+      List<Department> list = new ArrayList<>();
+
+      while (rs.next()) {
+        Department dep = new Department();
+        dep.setId(rs.getInt("Id"));
+        dep.setName(rs.getString("Name"));
+        list.add(dep);
+      }
+      return list;
+    } catch (SQLException e) {
+      throw new DbException(e.getMessage());
+    } finally {
+      DB.closeStatement(st);
+      DB.closeResultSet(rs);
+    }
   }
   
 }
